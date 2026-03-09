8398dd5e04b7d4a988182ee82b64dadf5395946f
JoaoGabriel038 <joaogabriel038@gmail.com>
2026-03-09 11:22:51 -0300

    fiap

---
+++ bool.py
@@ -0,0 +1,5 @@
+texto = "lorem ipsum dolor  sit amet lorem"
+
+print(texto.capitalize())
+print(texto.upper())
+print(texto.lower())
---
+++ calculadora.py
@@ -0,0 +1,13 @@
+print("Calculadora")
+primeiro_valor = int(input("Digite o primeiro valor: "))
+segundo_valor = int(input("Digite o segundo valor: "))
+
+soma = primeiro_valor + segundo_valor
+subtracao = primeiro_valor - segundo_valor
+divisao = primeiro_valor / segundo_valor
+multiplicacao = primeiro_valor * segundo_valor
+
+print(f"resultado {soma}")
+print(f"resultado {subtracao}")
+print(f"resultado {divisao}")
+print(f"resultado {multiplicacao}")
